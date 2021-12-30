c85beb117a3bf434cd3ead1505b4b525d06858ea
David Bentham <db260179@gmail.com>
2021-12-30 16:51:45 +0000

    Add custom build.sh and docker build image and scripts - exploit access for Xiaomi Mi4ag gigabit model
    Signed-off-by: David Bentham <db260179@gmail.com>

--- build.sh
+++ build.sh
@@ -10,38 +10,9 @@ echo "Install all packages from feeds..."
 ./scripts/feeds install -a && ./scripts/feeds install -a
 
 echo "Copy Openwrt official config..."
-release=$(grep -m1 '$(VERSION_REPO),' include/version.mk | awk -F, '{ print $3 }' | sed 's/[)]//g' | tr [:upper:] [:lower:])
+release=$(grep -m1 '$(VERSION_NUMBER),' include/version.mk | awk -F, '{ print $3 }' | sed 's/[)]//g')
 
-wget $release/targets/ramips/mt7621/config.buildinfo -O .config
-
-echo "Set to use default config"
-make defconfig
-
-echo "Download packages before build"
-if [ "$opt" = "nodownload" ]; then
-   echo "Skipping download of packages.."
-else
-   make download
-fi
-
-echo "Start build and log to build.log"
-make -j$(($(nproc)+1)) V=s CONFIG_DEBUG_SECTION_MISMATCH=y 2>&1 | tee build.log
-}
-
-build-custom () {
-echo "Update feeds..."
-./scripts/feeds update -a
-
-echo "Install all packages from feeds..."
-./scripts/feeds install -a && ./scripts/feeds install -a
-
-if [ -f "Custom.config" ]; then
-   echo "Copying Custom Openwrt config..."
-   cp Custom.config .config
-else
-   echo "Custom.config does no exit! - Please copy you custom config first!"
-   exit 1
-fi
+wget https://downloads.openwrt.org/releases/$release/targets/ramips/mt7621/config.buildinfo -O .config
 
 echo "Set to use default config"
 make defconfig
@@ -81,9 +52,6 @@ case "$1" in
   build-official)
     build-official
     ;;
-  build-custom)
-    build-custom
-    ;;
   build-rebuild)
     build-rebuild
     ;;
@@ -97,9 +65,8 @@ case "$1" in
     clean-full
     ;;
   *)
-    echo "Usage: $0 {build-official|build-custom|build-rebuild|build-rebuild-ignore|clean-min|clean-full}" >&2
+    echo "Usage: $0 {build-official|build-rebuild|build-rebuild-ignore|clean-min|clean-full}" >&2
     echo "build-official: {Openwrt standard config}" >&2
-    echo "build-custom: {Custom config}" >&2
     echo "Optional: {nodownload = No downloads of packages}" >&2
     exit 1
     ;;
