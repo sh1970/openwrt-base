9038b9c448c07ab60121ebc39e3b13ceb93cfc81
db260179 <db260179@gmail.com>
2023-10-29 17:49:25 +0000

    docker: Improve tftpserver.sh and add docker-compose file to run if docker-compose command is available

--- docker/tftpserver.sh
+++ docker/tftpserver.sh
@@ -1,23 +1,53 @@
 #!/bin/bash
 #set -x
 
-ARGS="--rm --name mi4a-openwrt-recovery -d --cap-add NET_ADMIN --network host -v $PWD/dnsmasq.conf:/etc/dnsmasq.conf -v $PWD/tftpboot:/tftpboot"
+export IMAGE="openwrt-dnsmasq:latest"
+export CONTAINERNAME="tftpboot-server"
 
-case "$1" in
-  build)
-    docker build -t mi4a-dnsmasq:latest -f Dockerfile.dnsmasq .
-    ;;
-  start)
-    docker run $ARGS mi4a-dnsmasq:latest
-    ;;
-  stop)
-    docker stop mi4a-openwrt-recovery
-    ;;
-  restart)
-    docker restart mi4a-openwrt-recovery
-    ;;
-  *)
-    echo "Usage: $0 {build|start|stop|restart}" >&2
-    exit 1
-    ;;
-esac
+ARGS="--rm --name tftpboot-server -d --cap-add NET_ADMIN --network host -v $PWD/dnsmasq.conf:/etc/dnsmasq.conf -v $PWD/tftpboot:/tftpboot"
+
+if command -v docker-compose &> /dev/null; then
+  # If docker-compose exists, use it
+  case "$1" in
+    build)
+      docker-compose build
+      ;;
+    config)
+      docker-compose config
+      ;;
+    start)
+      docker-compose up -d
+      ;;
+    stop)
+      docker-compose down
+      ;;
+    restart)
+      docker-compose restart $CONTAINERNAME
+      ;;
+    *)
+      echo "Usage: $0 {build|config|start|stop|restart}" >&2
+      exit 1
+      ;;
+  esac
+else
+  # If docker-compose doesn't exist, run other commands
+  case "$1" in
+    build)
+      docker build -t $IMAGE -f Dockerfile.dnsmasq .
+      ;;
+    start)
+      docker run $ARGS $IMAGE
+      ;;
+    stop)
+      docker stop $CONTAINERNAME
+      ;;
+    restart)
+      docker restart $CONTAINERNAME
+      ;;
+    *)
+      echo "Usage: $0 {build|start|stop|restart}" >&2
+      exit 1
+      ;;
+  esac
+fi
+
